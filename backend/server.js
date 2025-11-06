9d954f5eee0a8b409b4bdb4cc6344dfc307f720c
Zavy Chapoñan <zchaponanramirez@gmail.com>
2025-11-06 11:49:59 -0500

    Modifique el IP

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: 'http://localhost:5173',
+   origin: ['http://localhost:5173', 'http://192.168.1.8:5173'],
   credentials: true
 }));
 app.use(express.json());
