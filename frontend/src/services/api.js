4192a32d22f63da711c901bbfcff27b0fbe21fe2
Jhanire Mendoza <jhanire.mendoza@unmsm.edu.pe>
2025-11-06 11:51:22 -0500

    Mod de IP

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "http://192.168.56.1:5000/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
